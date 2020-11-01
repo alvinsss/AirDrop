1973c0718fb636f5a975f6c99b6bdc99a9da1801
alvinsss <6449694@qq.com>
2020-11-01 21:50:37 +0800

    去掉无用代码

--- tests/sidc.py
+++ tests/sidc.py
@@ -7,18 +7,7 @@
 import  time as t
 import random
 from selenium import webdriver
-from selenium.webdriver import ActionChains
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium import webdriver
 from utils.public import data_dir_file
-from PIL import Image
-from io import BytesIO
-import re
-import numpy as np
-from io import BytesIO
-import time, requests
 
 
 class Test_Airdrop():
@@ -48,6 +37,7 @@ class Test_Airdrop():
 
     def Participate_Airdrop(self):
         fmt='%Y-%m-%d %H:%M:%S'      #定义时间显示格式
+        count=0
         dr = webdriver.Chrome(self.drexepath)
         dr.get(self.mmurl)
         dr.implicitly_wait(5)
@@ -56,22 +46,21 @@ class Test_Airdrop():
         jsCode = "var q=document.documentElement.scrollTop=1200"
         dr.execute_script(jsCode)
         eth_ads = Test_Airdrop.get_ETH_Address(self)
-        count=0
+
         for address in eth_ads:
             #处理提交数据请求
             count = count+1
-            Date=t.strftime(fmt,t.localtime(t.time()))     #把传入的元组按照格式，输出字符串
-            print(Date+ ":current address->"+address+"__NO is->"+str(count)+" _submit is start ....")
             dr.find_element_by_xpath("//*[@type='text']").clear()
-            t.sleep(random.randrange(20,50))#随机20-50秒之间
+            t.sleep(random.randrange(1,3))#随机9-61秒之间
             dr.find_element_by_xpath("//*[@type='text']").send_keys(address)
+            print(t.strftime(fmt,t.localtime(t.time()))+":NO is:"+str(count)+ ",current address->"+address+"  submit is start ....")
+            t.sleep(random.randrange(9,61))#随机9-61秒之间
             dr.find_element_by_xpath("//body/a[@id='airdrop']/div[1]/div[@class='container']/div[@class='row c-row1']/div[@class='col-md-2']/p[1]").click()
-            t.sleep(random.randrange(5, 9, 2))#随机
-            print("current address->"+address+"__NO is->"+str(count)+" _submit is  end.")
-
+            dr.find_element_by_xpath("//*[@type='text']").send_keys(address)
+            print("current address->"+address+"_submit is end")
+        print("total:"+count+"  address tranf over !")
         dr.close()
 
 if __name__ == '__main__':
     Test_Ad = Test_Airdrop()
-    # Test_Ad.get_ETH_Address()
     Test_Ad.Participate_Airdrop()
