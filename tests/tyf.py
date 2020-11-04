ed657dc4a8ae216d73ef86d5be612be8d9d18884
alvinsss <6449694@qq.com>
2020-11-04 14:46:50 +0800

    https://www.tyfcoin.com  太姨夫空投，满20人

--- tests/tyf.py
+++ tests/tyf.py
@@ -21,9 +21,9 @@ class Test_Airdrop():
 
     def __init__(self):
         '''初始配置'''
-        self.filename = data_dir_file(dirPath='SIDC',fileName="ETH_ad.txt")
+        self.filename = data_dir_file(dirPath='TYF',fileName="ETH_ad.txt")
         self.drexepath = get_extend_chromedriver_file()
-        self.url = "http://www.sidc.pro"
+        self.url = "https://www.tyfcoin.com/index/index/index/code/692"
         self.log = Logger()
 
     def get_ETH_Address(self):
@@ -40,8 +40,8 @@ class Test_Airdrop():
 
     def Participate_Airdrop(self):
         '''空投页面提交eth地址'''
+        count = 0
         fmt='%Y-%m-%d %H:%M:%S'      #定义时间显示格式
-        count=0
         dr = webdriver.Chrome(self.drexepath)
         dr.get(self.url)
         dr.implicitly_wait(5)
@@ -53,15 +53,24 @@ class Test_Airdrop():
 
         for address in eth_ads:
             #处理没一行eth地址数据
+            dr = webdriver.Chrome(self.drexepath)
+            dr.refresh()
+            dr.get(self.url)
+            dr.implicitly_wait(5)
+            dr.maximize_window()
+            t.sleep(random.randrange(3, 5, 1))
+            jsCode = "var q=document.documentElement.scrollTop=1200"
+            dr.execute_script(jsCode)
             count = count+1
             dr.find_element_by_xpath("//*[@type='text']").clear()
             t.sleep(random.randrange(1,2))
             dr.find_element_by_xpath("//*[@type='text']").send_keys(address)
-            # print(t.strftime(fmt,t.localtime(t.time()))+":NO is:"+str(count)+ ",current address->"+address+"  submit is start ....")
+            print(t.strftime(fmt,t.localtime(t.time()))+":NO is:"+str(count)+ ",current address->"+address+"  submit is start ....")
             # self.log.info(t.strftime(fmt,t.localtime(t.time()))+":NO is:"+str(count)+ ",current address->"+address+"  submit is start ....")
-            t.sleep(random.randrange(2,62))#随机9-61秒之间
-            dr.find_element_by_xpath("//body/a[@id='airdrop']/div[1]/div[@class='container']/div[@class='row c-row1']/div[@class='col-md-2']/p[1]").click()
-            self.log.info(t.strftime(fmt,t.localtime(t.time()))+"_SIDC_current address->"+address+"_submit is end")
+            t.sleep(random.randrange(5,110))#随机9-61秒之间
+            dr.find_element_by_id("btn").click()
+            self.log.info(t.strftime(fmt,t.localtime(t.time()))+"_TYF_current address->"+address+"_submit is end")
+            dr.close()
         self.log.info("total:"+count+"  address tranf over !")
         dr.close()
 
